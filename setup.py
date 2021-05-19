a12d0c0c18003375fda729636c561dd34da05cde
Kristian Klemon <klemon@unternehmertum.de>
2021-05-19 10:22:20 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+from setuptools import setup, find_packages
+
+ROOT = Path(__file__).parent
+README = (ROOT / 'README.md').read_text()
+
+setup(
+    name='gon-pytorch',
+    packages=find_packages(),
+    version='0.1.0',
+    license='MIT',
+    description='Implementation of Gradient Origin Networks in PyTorch',
+    long_description=README,
+    long_description_content_type='text/markdown',
+    author='Kristian Klemon',
+    author_email='kristian.klemon@gmail.com',
+    url='https://github.com/kklemon/gon-pytorch',
+    keywords=['artificial intelligence', 'deep learning'],
+    install_requires=['torch']
+)
